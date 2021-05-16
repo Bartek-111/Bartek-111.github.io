4168030bb252f3b571f740f459118c56e5497254
Bartek-111 <gromulskibartek@gmail.com>
2021-05-16 15:48:52 +0200

    mm

--- script.js
+++ script.js
@@ -2,14 +2,13 @@ console.log("hello world")
 let ania = { age: 12 };
 let basia = { age: 22 };
 let maciek = { age: 18 };
-function checkIsLegalToDrink(person){
+function checkIsLegalToDrink(person) {
     // jezeli wiecej lub rowno niz 18 napisz legalne
     // jezeli nie to napisz niestety brakuje ci jeszcze x lat
-    if(person.age >= 18){
+    if (person.age >= 18) {
         console.log("legalne")
     }
-    if(person.age < 18)
-    {
+    if (person.age < 18) {
         let diffrence = 18 - person.age;
         console.log(`niestety brakuje ci jeszcze ${diffrence} lat`)
     }
@@ -21,17 +20,17 @@ checkIsLegalToDrink(ania);
 console.log("maciek")
 checkIsLegalToDrink(maciek);
 
-function changeElementByPerson(person){
+function changeElementByPerson(person) {
     //document.getElementById("person").innerText
-    if(person.age >= 18)   {
+    if (person.age >= 18) {
         let element = document.getElementById("person");
-        element.innerText ="legalne"
-    } 
-    if(person.age < 18)   {
+        element.innerText = "legalne"
+    }
+    if (person.age < 18) {
         let element = document.getElementById("person");
         let diffrence = 18 - person.age;
-        element.innerText =`niestety brakuje ci jeszcze ${diffrence} lat`
-    } 
+        element.innerText = `niestety brakuje ci jeszcze ${diffrence} lat`
+    }
 }
 
 document.getElementById("maciek").onclick = () => {
@@ -47,49 +46,49 @@ document.getElementById("check").onclick = () => {
     let input = document.getElementById("wiek");
     let rawValue = input.value;
     let wiek = parseInt(rawValue);
-    if(isNaN(wiek)){
+    if (isNaN(wiek)) {
         let element = document.createElement("p");
         let historyElement = document.getElementById("history");
         element.classList.add("error")
         element.innerText = `Błędna wartość w polu sprawdź wiek '${rawValue}' nie jest prawidłowym wiekiem`
         historyElement.insertBefore(element, historyElement.firstChild)
-        setTimeout(()=>{
+        setTimeout(() => {
             historyElement.removeChild(element)
         }, 10 * 1000);
         return;
     }
-    if(wiek < 0)
-    {let element = document.createElement("p");
-    let historyElement = document.getElementById("history");
-    element.classList.add("error")
+    if (wiek < 0) {
+        let element = document.createElement("p");
+        let historyElement = document.getElementById("history");
+        element.classList.add("error")
         element.innerText = `Błędna wartość w polu sprawdź wiek '${rawValue}' jest ujemna`
         historyElement.insertBefore(element, historyElement.firstChild)
-        setTimeout(()=>{
+        setTimeout(() => {
             historyElement.removeChild(element)
         }, 10 * 1000);
         return;
 
 
     }
-    if(wiek >= 18){
+    if (wiek >= 18) {
         let element = document.createElement("p");
         let historyElement = document.getElementById("history");
         element.classList.remove("error")
         element.innerText = "legalne"
         historyElement.insertBefore(element, historyElement.firstChild)
-        setTimeout(()=>{
+        setTimeout(() => {
             historyElement.removeChild(element)
         }, 10 * 1000);
         return;
     }
-    if(wiek < 18){
+    if (wiek < 18) {
         let element = document.createElement("p");
         let historyElement = document.getElementById("history");
         let diffrence = 18 - wiek;
         element.classList.remove("error")
-        element.innerText =`niestety brakuje ci jeszcze ${diffrence} lat` 
+        element.innerText = `niestety brakuje ci jeszcze ${diffrence} lat`
         historyElement.insertBefore(element, historyElement.firstChild)
-        setTimeout(()=>{
+        setTimeout(() => {
             historyElement.removeChild(element)
         }, 10 * 1000);
         return;
