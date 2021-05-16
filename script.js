6159a3d2fa0e716f345d1dbb065c39f6455dbe1a
Bartek-111 <gromulskibartek@gmail.com>
2021-05-16 15:45:09 +0200

    m

--- script.js
+++ script.js
@@ -48,30 +48,50 @@ document.getElementById("check").onclick = () => {
     let rawValue = input.value;
     let wiek = parseInt(rawValue);
     if(isNaN(wiek)){
-        let element = document.getElementById("person");
+        let element = document.createElement("p");
+        let historyElement = document.getElementById("history");
         element.classList.add("error")
         element.innerText = `Błędna wartość w polu sprawdź wiek '${rawValue}' nie jest prawidłowym wiekiem`
+        historyElement.insertBefore(element, historyElement.firstChild)
+        setTimeout(()=>{
+            historyElement.removeChild(element)
+        }, 10 * 1000);
         return;
     }
     if(wiek < 0)
-    {let element = document.getElementById("person");
-        element.classList.add("error")
+    {let element = document.createElement("p");
+    let historyElement = document.getElementById("history");
+    element.classList.add("error")
         element.innerText = `Błędna wartość w polu sprawdź wiek '${rawValue}' jest ujemna`
+        historyElement.insertBefore(element, historyElement.firstChild)
+        setTimeout(()=>{
+            historyElement.removeChild(element)
+        }, 10 * 1000);
         return;
 
 
     }
     if(wiek >= 18){
-        let element = document.getElementById("person");
+        let element = document.createElement("p");
+        let historyElement = document.getElementById("history");
         element.classList.remove("error")
         element.innerText = "legalne"
+        historyElement.insertBefore(element, historyElement.firstChild)
+        setTimeout(()=>{
+            historyElement.removeChild(element)
+        }, 10 * 1000);
         return;
     }
     if(wiek < 18){
-        let element = document.getElementById("person");
+        let element = document.createElement("p");
+        let historyElement = document.getElementById("history");
         let diffrence = 18 - wiek;
         element.classList.remove("error")
         element.innerText =`niestety brakuje ci jeszcze ${diffrence} lat` 
+        historyElement.insertBefore(element, historyElement.firstChild)
+        setTimeout(()=>{
+            historyElement.removeChild(element)
+        }, 10 * 1000);
         return;
 
     }
